46923ea067f4cdc58ce6c113af8a3d2a894f6093
103D <diar20190529@gmail.com>
2026-03-19 14:51:02 +0500

    fix python module import path for vercel

--- api/index.py
+++ api/index.py
@@ -1,2 +1,9 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+
 from app.main import app
 handler = app
